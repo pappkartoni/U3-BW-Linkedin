3c62a082a1f10aee32762edd46ea7b70632eb73b
Florian Dreyer <flodreyer@web.de>
2023-02-20 14:01:10 +0100

    i done fucked up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/css/style.css"
-import Navi from './components/Navi';
 import { BrowserRouter } from 'react-router-dom';
+import { Container, Row, Col } from "react-bootstrap"
+import Navi from './components/Navi';
 import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <BrowserRouter>
       <Navi />
       <main>
+        <Container>
+          <Row>
+            <Col md={8}>
+            </Col>
+            <Col md={4}>
+              <Sidebar />
+            </Col>
+          </Row>
+        </Container>
         <Footer />
-
       </main>
     </BrowserRouter>
   );
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+
+const Sidebar = () => {
+
+    return (
+        <div className="d-flex flex-column">
+            
+        </div>
+    )
+}
+
+export default Sidebar
