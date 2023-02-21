dd536067b65f96c22c2fb5016c8e0e13b78cd761
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-02-21 15:21:43 +0100

    get experience data

--- src/components/ExperienceTile.jsx
+++ src/components/ExperienceTile.jsx
@@ -1,19 +1,25 @@
+import { format, parseISO } from "date-fns";
+
 const ExperienceTile = (props) => {
-    return (
-        <div className="experience d-flex">
-            <div>
-                <img src={props.exp.image} alt={props.exp.company} />
-            </div>
-            <div className="d-flex flex-column">
-                <h3>{props.exp.role}</h3>
-                <h4>{props.exp.company}</h4>
-                <span>{props.exp.startDate} - {props.exp.endDate}</span>
-                <span>{props.exp.area}</span>
-                <p>{props.exp.description}</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="experience d-flex">
+      <div>
+        <img src={props.exp.image} alt={props.exp.company} />
+      </div>
+      <div className="d-flex flex-column">
+        <h3>{props.exp.role}</h3>
+        <h4>{props.exp.company}</h4>
+        <span>
+          {props.exp.startDate &&
+            format(parseISO(props.exp.startDate), "MMMM yyyy")}{" "}
+          {props.exp.endDate &&
+            format(parseISO(props.exp.endDate), " - MMMM yyyy")}
+        </span>
+        <span>{props.exp.area}</span>
+        <p>{props.exp.description}</p>
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default ExperienceTile
+export default ExperienceTile;
