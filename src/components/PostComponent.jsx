b900033fc0754c5cebc08f7cf5fe8b8c6c31f3e5
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-02-22 17:10:18 +0100

    Single Post designed

--- src/components/PostComponent.jsx
+++ src/components/PostComponent.jsx
@@ -16,9 +16,12 @@ const PostComponent = () => {
     <Container>
       {posts.length > 0 && (
         <>
-          {posts.map((post) => (
-            <SinglePost key={post._id} post={post} />
-          ))}
+          {posts
+            .slice(0, 20)
+            .reverse()
+            .map((post) => (
+              <SinglePost key={post._id} post={post} />
+            ))}
         </>
       )}
     </Container>
