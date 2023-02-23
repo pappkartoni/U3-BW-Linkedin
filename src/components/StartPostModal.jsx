6970f14972c101f99133aa2f7375e5bd1f30fd81
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-02-23 18:03:48 +0100

    fixing post modal style

--- src/components/StartPostModal.jsx
+++ src/components/StartPostModal.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
-import { Button, Modal } from "react-bootstrap";
+import { Button, Col, Modal, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { createPost, getAllPosts } from "../redux/actions";
 import "../assets/css/StartPostModal.css";
 import globalImage from "../assets/img/developer1.jfif";
+import { RiGalleryFill, RiMoreFill } from "react-icons/ri";
+import { FaVideo } from "react-icons/fa";
+import { FaDochub } from "react-icons/fa";
 
 const StartPostModal = ({ show, handleClose }) => {
   const image = useSelector((state) => state.getProfile.fetchProfile);
@@ -17,7 +20,7 @@ const StartPostModal = ({ show, handleClose }) => {
   const submitPost = () => {
     dispatch(createPost({ text }, handleClose));
     dispatch(getAllPosts());
-    setText("")
+    setText("");
   };
   return (
     <Modal show={show} onHide={handleClose}>
@@ -44,34 +47,61 @@ const StartPostModal = ({ show, handleClose }) => {
         ></textarea>
       </Modal.Body>
 
-      <Modal.Footer className="start-post-modal-footer">
-        <div>
-          <i class="bi bi-image"></i>
-        </div>
-        <div>
-          <i class="bi bi-play-btn-fill"></i>
-        </div>
-        <div>
-          <i class="bi bi-sticky-fill"></i>
-        </div>
-        <div>
-          <i class="bi bi-three-dots"></i>
-        </div>
-        <div className="modal-footer-breakline"></div>
-        <div className="start-post-modal-anyone-btn">
-          <i class="bi bi-chat-right-text"></i>
-          <span>Anyone</span>
-        </div>
-        <div>
-          <i class="bi bi-clock"></i>
-        </div>
-        <Button
-          variant={text.length == 0 ? "secondary" : "primary"}
-          disabled={text.length == 0}
-          onClick={submitPost}
-        >
-          Post (EDIT HOVER)
-        </Button>
+      <Modal.Footer className="justify-content-start ">
+        <Row className="w-100 align-items-center">
+          <Col md={8} className="px-0 ">
+            <div className="d-flex align-items-center">
+              <div className="d-flex align-items-center ">
+                <div className="icon-hover d-flex align-items-center jusrify-content-center ">
+                  {" "}
+                  <RiGalleryFill
+                    size={20}
+                    fill="rgba(0,0,0,0.6)"
+                    className="w-100"
+                  />
+                </div>
+                <div className="icon-hover d-flex align-items-center jusrify-content-center ">
+                  {" "}
+                  <FaVideo size={20} fill="rgba(0,0,0,0.6)" className="w-100" />
+                </div>
+                <div className="icon-hover d-flex align-items-center jusrify-content-center ">
+                  {" "}
+                  <FaDochub
+                    size={20}
+                    fill="rgba(0,0,0,0.6)"
+                    className="w-100"
+                  />
+                </div>
+                <div className="icon-hover d-flex align-items-center jusrify-content-center ">
+                  {" "}
+                  <RiMoreFill
+                    size={20}
+                    fill="rgba(0,0,0,0.6)"
+                    className="w-100"
+                  />
+                </div>
+              </div>
+              <div className="modal-footer-breakline"></div>
+              <div className="start-post-modal-anyone-btn ml-2">
+                <i class="bi bi-chat-right-text"></i>
+                <span>Anyone</span>
+              </div>
+            </div>
+          </Col>
+          <Col md={4}>
+            <div className="d-flex align-items-center justify-content-end">
+              <i className="bi bi-clock"></i>
+              <Button
+                variant={text.length === 0 ? "secondary" : "primary"}
+                disabled={text.length === 0}
+                onClick={submitPost}
+                className="ml-2"
+              >
+                Post
+              </Button>
+            </div>
+          </Col>
+        </Row>
       </Modal.Footer>
     </Modal>
   );
